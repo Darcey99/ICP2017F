bb566d873983411992fa14f42480fe1a675c1803
Darcey99 <31543150+Darcey99@users.noreply.github.com>
2017-11-13 09:52:51 -0600

    Delete timeFacFunc.m

--- Quiz/4/timeFacFunc.m
+++
@@ -1,5 +0,0 @@
-function output = timeFacFunc
-disp([char(9),'average runtime for getFacWhile(1700000): ',num2str(timeit(@()getFacWhile(1700000))), 'seconds']);
-disp([char(9),'average runtime for getFacFor(1700000): ',num2str(timeit(@()getFacFor(1700000))), 'seconds']);
-disp([char(9),'average runtime for getFacVec(1700000): ',num2str(timeit(@()getFacVec(1700000))), 'seconds']);
-end
