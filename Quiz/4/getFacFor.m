80f2ec7724c5f5b1aa817ff2697cec2c8b134a06
Darcey99 <31543150+Darcey99@users.noreply.github.com>
2017-11-13 09:32:22 -0600

    Add files via upload

---
+++ Quiz/4/getFacFor.m
@@ -0,0 +1,8 @@
+function output = getFacFor(n)
+d = 3;
+output = 1;
+    for i = n:-1:1
+    output = output*i;
+    end
+end
+
---
+++ Quiz/4/getFacWhile.m
@@ -0,0 +1,8 @@
+function d = getFacWhile(n)
+    d = 1;
+    i = n;
+        while i > 0
+            d = d * i;
+            i = i - 1;
+        end
+end  
